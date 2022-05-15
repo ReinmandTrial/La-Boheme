c2acc31844545cda885a548a5518181cb31a8596
spjustice <sanyok.vergun@gmail.com>
2022-05-16 02:09:18 +0300

    f

--- src/js/main.js
+++ src/js/main.js
@@ -21,9 +21,32 @@ imagesLargeFade.forEach(imageLargeFade => {
    imageLargeFade.querySelector('.image-large__slide').setAttribute('data-aos', arr[rand])
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+const imagesFade = document.querySelectorAll('.image')
+imagesFade.forEach(imageFade => {
+   let arr = ['slide-down', 'slide-left', 'slide-right', 'slide-up'];
+   let rand = Math.floor(Math.random() * arr.length);
+   imageFade.querySelector('.image__item').setAttribute('data-aos', arr[rand])
+});
+const imagesLarge = document.querySelectorAll('.image-large')
+imagesLarge.forEach(imageLarge => {
+   // let arrMargin = ['mt-100', 'mt-0'];
+   // let randMargin = Math.floor(Math.random() * arrMargin.length);
+   // imageLarge.classList.add(arrMargin[randMargin])
+   let arr = ['image-large--1', 'image-large--2', 'image-large--3', 'image-large--4', 'image-large--5', 'image-large--6', 'image-large--7'];
+   let rand = Math.floor(Math.random() * arr.length);
+   imageLarge.classList.add(arr[rand])
+});
 
-   var searchBtns = document.querySelectorAll('.search__btn')
+const images = document.querySelectorAll('.image')
+images.forEach(image => {
+   let arrMargin = ['mt-100', 'mt-0'];
+   let randMargin = Math.floor(Math.random() * arrMargin.length);
+   image.classList.add(arrMargin[randMargin])
+   let arr = ['image--1', 'image--2', 'image--3', 'image--4', 'image--5', 'image--6'];
+   let rand = Math.floor(Math.random() * arr.length);
+   image.classList.add(arr[rand])
+});
+document.addEventListener('DOMContentLoaded', function () {
 
    document.addEventListener('mousemove', (e) => {
       const logos = document.querySelectorAll('.logo')
@@ -36,11 +59,13 @@ document.addEventListener('DOMContentLoaded', function () {
       });
 
    });
+   var searchBtns = document.querySelectorAll('.search__btn')
    searchBtns.forEach(searchBtn => {
       searchBtn.addEventListener('click', function () {
          this.closest('.search').classList.toggle('active')
       })
    });
+
 
    function logoRotate() {
       document.querySelector('.logo__wrap').classList.remove('animate');
@@ -52,14 +77,43 @@ document.addEventListener('DOMContentLoaded', function () {
    // logoRotate()
    setInterval(logoRotate, 5000);
 
-   const imagesLarge = document.querySelectorAll('.image-large')
-   imagesLarge.forEach(imageLarge => {
-      let arr = ['image-large--1', 'image-large--2', 'image-large--3', 'image-large--4', 'image-large--5', 'image-large--6', 'image-large--7'];
-      let rand = Math.floor(Math.random() * arr.length);
-      imageLarge.classList.add(arr[rand])
+
+
+
+   const catalog = new Swiper('.catalog-slider', {
+      navigation: {
+         nextEl: '.catalog-slider__btn-next',
+         prevEl: '.catalog-slider__btn-prev',
+      },
+      breakpoints: {
+         1100: {
+            slidesPerView: 3,
+
+         },
+         768: {
+         },
+         320: {
+            slidesPerView: 2,
+         }
+      },
+      // autoplay: {
+      //    delay: 3700,
+      //    disableOnInteraction: false,
+      // },
+      simulateTouch: false,
+      slidesPerView: 3,
+      // loop: true,
+      speed: 1000
    });
-
-
+   // setTimeout(function () {
    AOS.init();
+   // }, 500)
+   var cards = document.querySelectorAll('.card')
+   cards.forEach(el => {
+      var contentHeight = el.querySelector('.card__content').clientHeight
+
+      el.style.height = contentHeight + 'px'
+
+   });
 })
 
