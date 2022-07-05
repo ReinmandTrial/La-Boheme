d753d763187680e6ac45ff9c95b78a0de9b252d1
spjustice <sanyok.vergun@gmail.com>
2022-07-05 23:29:35 +0300

    fix panorama initial pos, add social licks

--- dist/js/main.js
+++ dist/js/main.js
@@ -587,14 +587,19 @@ document.addEventListener('DOMContentLoaded', function () {
    viewer.OrbitControls.maxFov = 70;
    viewer.add(panorama);
 
-   viewer.OrbitControls.minPolarAngle = Math.PI / 2.2;
-   viewer.OrbitControls.maxPolarAngle = Math.PI * 2 / 3.7;
+   // viewer.OrbitControls.minPolarAngle = Math.PI / 2.2;
+   // viewer.OrbitControls.maxPolarAngle = Math.PI * 2 / 3.7;
    // viewer.OrbitControls.addEventListener('change', function () {
    //    // console.log('change');
    //    console.log(panorama.getZoomLevel());
    panorama.addEventListener('enter-fade-start', function () {
-      viewer.tweenControlCenter(new THREE.Vector3(2100, 0, 0), 0);
+      viewer.tweenControlCenter(new THREE.Vector3(2100, 0, -500), 0);
    });
+   $('.pt-trigger').on('click', function () {
+      viewer.tweenControlCenter(new THREE.Vector3(2100, 0, -500), 0);
+
+
+   })
 
    // })
    // panorama
